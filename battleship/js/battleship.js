da9c4d0d2f5ac766de00e1daf6a2f0ff94f8de6e
Shawn Pivonka <shawnpivonka@Shawns-MacBook-Pro.local>
2015-11-04 09:22:45 -0600

    added ship placement for player

--- battleship/js/battleship.js
+++ battleship/js/battleship.js
@@ -1,6 +1,8 @@
 "use strict"
 
 var width = $('#game_container').width();
+var numShips = 0;
+var playerTurn = false;
 
 function submitFire() {
 	var $target = $(event.target);
@@ -24,13 +26,50 @@ function randomSquare () {
 		$('#computer_table tr:eq(' + randRow +') td:eq(' + randCol + ')').addClass("ship");
 	}
 }
+function placeShip() {
+	$(event.target).css('background', 'pink');
+}
 
 
 $('#game_container').click(function() {
 
-	submitFire(event);
+	var targetTag = $(event.target).prop('tagName');
+	var tableId = $(event.target).parent('tr').parent('tbody').parent().prop('id');
+
+	if( targetTag === 'TD' && tableId === 'computer_table' && playerTurn){
+		submitFire();
+	}
+	if( targetTag === 'TD' && tableId === 'player_table' && numShips < 5){
+		placeShip();
+		numShips +=1;
+		if (numShips === 4){
+			playerTurn = true;
+		}
+	}
 });	
 
 
 $('table').css('height',width/2 + 'px');
 randomSquare();
+
+
+/******************************  Notes  and  other  stuff ****************************\
+Clicking on the #game_board causes it to change color, need to filter events before sending
+them to submitFire();
+
+should do space ships instead of battle ship shapes
+
+also you can select the hole table some how on a click near the edges
+
+
+
+
+
+*/
+
+
+
+
+
+
+
