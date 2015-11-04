0cf68be14d962a55efac92e5ca754412410f1170
Shawn Pivonka <shawnpivonka@Shawns-MacBook-Pro.local>
2015-11-03 18:08:14 -0600

    battleship first commit

---
+++ battleship/js/battleship.js
@@ -0,0 +1,6 @@
+"use strict"
+
+var width = $('#game_container').width();
+console.log(width);
+
+$('table').css('height',width/2 + 'px');
---
+++ battleship/js/vuePractice.js
@@ -0,0 +1,18 @@
+
+
+new Vue({
+	el: '#demo',
+	data:{
+
+		people: [
+			{
+				name: 'Shawn',
+				email: 'sprov'
+			},
+			{
+				name: 'jessica',
+				email: 'jbmaya'
+			}
+		]
+	}
+});
