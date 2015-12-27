9e8a3262728ea0cb1f7a118c6779dcacb1473924
Shawn Pivonka <shawnpivonka@shawns-mbp.gateway.pace.com>
2015-12-27 16:00:35 -0600

    made buttons in canvas instead of an overlayed div. makes for simpler programing of a controler in the futer. going to test the on my note 3

--- bubbletrouble/js/bubbletrouble.js
+++ bubbletrouble/js/bubbletrouble.js
@@ -49,53 +49,6 @@ var counter = 0;
 		(event === 37) && gameObject.stopMovingLeft();
 	}
 
- 
-// ********************  Controller listeners  ************************\\
-
-	$('#dPadUp').on("mousedown", function(){
-		gameObject.jump();		
-	});
-
-	$('#dPadLeft').on("mousedown", function(){
-		if (gameObject.movingLeftFlag === false) {
-			gameObject.stopMovingRight();
-			square.ft = 15;
-			square.ftMax = 216;
-			square.ftSize = 1;
-			gameObject.movingLeftFlag = true;	
-		}		
-	});
-
-	$('#dPadRight').on("mousedown", function(){
-		if (gameObject.movingRightFlag === false){
-			gameObject.stopMovingLeft();
-			square.ft = 15;
-			square.ftMax = 216;
-			square.ftSize = 1;
-			gameObject.movingRightFlag = true;
-		}		
-	});
-
-	$('#dPadDown').on("mousedown", function(){
-		new gameObject.fire();		
-	});
-
-	$('#dPadUp').on("mouseup", function(){
-				
-	});
-
-	$('#dPadLeft').on("mouseup", function(){
-		gameObject.stopMovingLeft();	
-	});
-
-	$('#dPadRight').on("mouseup", function(){
-		gameObject.stopMovingRight();
-	});
-
-	$('#dPadDown').on("mouseup", function(){
-				
-	});
-
 
 		//************************  Variable Declarations  ****************\\
 
@@ -107,7 +60,7 @@ var counter = 0;
 	var c = document.getElementById("myCanvas");
 	var ctx = c.getContext("2d");
 
-	var controller = document.getElementById("myController");
+	// var controller = document.getElementById("myController");
 
 
 	/**
@@ -125,7 +78,7 @@ var counter = 0;
 	 *  Set cSizing (canvas sizing) off the smallest dimention / 100.	
 	 *	cSizing is just a canvas unit used to make game scaleable.
 	 */
-	var cSizing = (cx <= cy) ? cx : cy ;
+	var cSizing = cy;
 	cSizing *= .01;
 	cy *= .01;
 	cx *= .01;
@@ -135,30 +88,185 @@ var counter = 0;
 	 *
 	 *
 	 */
-	controller.style.width = String(ctx.canvas.width) + "px";
-	controller.style.height = String(ctx.canvas.width * .45) + "px";
-	controller.style.top = "1px";
-	controller.style.left = "1px";
-	document.getElementById('dPadUp').style.left    = String(81 * cx) + "px";
-	document.getElementById('dPadUp').style.top     = String(40 * cy) + "px";
-	document.getElementById('dPadLeft').style.left  = String(5 * cx) + "px";
-	document.getElementById('dPadLeft').style.top   = String(50 * cy) + "px";
-	document.getElementById('dPadRight').style.left = String(16.5 * cx) + "px";
-	document.getElementById('dPadRight').style.top  = String(50 * cy) + "px";
-	document.getElementById('dPadDown').style.left  = String(80 * cx) + "px";
-	document.getElementById('dPadDown').style.top   = String(60 * cy) + "px";
+	// controller.style.width = String(ctx.canvas.width) + "px";
+	// controller.style.height = String(ctx.canvas.width * .45) + "px";
+	// controller.style.top = "1px";
+	// controller.style.left = "1px";
+	// document.getElementById('dPadUp').style.left    = String(200 * cSizing) + "px";
+	// document.getElementById('dPadUp').style.top     = String(40 * cSizing) + "px";
+	// document.getElementById('dPadLeft').style.left  = String(5 * cSizing) + "px";
+	// document.getElementById('dPadLeft').style.top   = String(50 * cSizing) + "px";
+	// document.getElementById('dPadRight').style.left = String(16.5 * cSizing) + "px";
+	// document.getElementById('dPadRight').style.top  = String(50 * cSizing) + "px";
+	// document.getElementById('dPadDown').style.left  = String(200 * cSizing) + "px";
+	// document.getElementById('dPadDown').style.top   = String(60 * cSizing) + "px";
+	// var buttons = document.getElementsByClassName('controller_button');
+
+ //    for (var i = 0; i < buttons.length; i++) {
+	// 	buttons[i].style.width = String(8 * cSizing) + "px";
+	// 	buttons[i].style.height= String(8 * cSizing) + "px";
+ //    }
+
+ // ********************  Controller listeners  ************************\\
+
+
+function handleStart(evt) {
+  evt.preventDefault();
+  var el = document.getElementById("myCanvas");
+  var touches = evt.changedTouches;
+  var yOff = -70;
+  var xOff = 0;
+
+  if(screen.width <= 768){
+
+  	xOff = -17;
+  } else if (screen.width <= 990) {
+
+  	xOff = -((screen.width - 722) /2) -14;
+  } else if (screen.width <= 1200) {
+
+  	xOff = -((screen.width - 942) /2) - 3;
+  }else {
+
+  	xOff = -((screen.width - 1142) / 2) - 2;
+  }
+        
+  for (var i = 0; i < touches.length; i++) {
+  // console.log("clientX:" + ((evt.changedTouches[i].clientX / cSizing) + xOff) * cSizing);
+  // console.log("clientY:" + ((evt.changedTouches[i].clientY / cSizing) + yOff) * cSizing);
+  console.log("clientY:" + (evt.changedTouches[i].pageY + yOff));
+  console.log("clientX:" + (evt.changedTouches[i].pageX + xOff));
+  console.log(xOff);
+  	for (var j = 0; j < gameObject.buttons.length; j++){
+  		console.log("left:" + gameObject.buttons[j].left());
+  		console.log("right:" + gameObject.buttons[j].right());
+  		console.log("top:" + gameObject.buttons[j].top());
+  		console.log("bottom:" + gameObject.buttons[j].bottom());
+  		if (gameObject.buttons[j].left()  < evt.changedTouches[i].pageX + xOff && 
+  			gameObject.buttons[j].right() > evt.changedTouches[i].pageX + xOff &&
+  			gameObject.buttons[j].top()   < evt.changedTouches[i].pageY + yOff &&
+  			gameObject.buttons[j].bottom()> evt.changedTouches[i].pageY + yOff)
+  		{
+  			var startAction = gameObject.buttons[j].startAction;
+  			var endAction = gameObject.buttons[j].endAction;
+  			gameObject[startAction]();
+  			gameObject.ongoingTouches[evt.changedTouches[i].identifier] = endAction;
+  		}
+  	}
+  	 console.log(evt.changedTouches[0]);
+
+    // console.log("touchstart:");
+    // ongoingTouches.push(copyTouch(touches[i]));
+    // var color = colorForTouch(touches[i]);
+    // ctx.beginPath();
+    // ctx.arc(touches[i].pageX, touches[i].pageY, 4, 0, 2 * Math.PI, false);  // a circle at the start
+    // ctx.fillStyle = color;
+    // ctx.fill();
+    // console.log("touchstart:");
+  }
+}
+
+function handleEnd(evt) {
+  evt.preventDefault();
+  console.log("touchended");
+  console.log(evt.changedTouches[0].identifier);
+  console.log(gameObject.ongoingTouches[evt.changedTouches[0].identifier]);
+  gameObject[gameObject.ongoingTouches[evt.changedTouches[0].identifier]]();
+  // gameObject[gameObject.ongoingTouches[0]]();
+  // var el = document.getElementsByTagName("canvas")[0];
+  // var ctx = el.getContext("2d");
+  // var touches = evt.changedTouches;
+
+  // for (var i = 0; i < touches.length; i++) {
+  //   var color = colorForTouch(touches[i]);
+  //   var idx = ongoingTouchIndexById(touches[i].identifier);
+
+  //   if (idx >= 0) {
+  //     ctx.lineWidth = 4;
+  //     ctx.fillStyle = color;
+  //     ctx.beginPath();
+  //     ctx.moveTo(ongoingTouches[idx].pageX, ongoingTouches[idx].pageY);
+  //     ctx.lineTo(touches[i].pageX, touches[i].pageY);
+  //     ctx.fillRect(touches[i].pageX - 4, touches[i].pageY - 4, 8, 8);  // and a square at the end
+  //     ongoingTouches.splice(idx, 1);  // remove it; we're done
+  //   } else {
+  //     log("can't figure out which touch to end");
+  //   }
+  // }
+}
+
+
+
+  var el = document.getElementById("myCanvas");
+  el.addEventListener("touchstart",handleStart, false);
+  // el.addEventListener("mousedown",handleStart,false);
+  // $('myCanvas').on("mousedown", function () {
+  // 	console.log("you clicked on the canvas game");
+  // });
+  el.addEventListener("touchend", handleEnd, false);
+  // el.addEventListener("touchcancel", handleCancel, false);
+  // el.addEventListener("touchmove", handleMove, false);
 
 	var gameObject = {
 
 		collisionCheckArray: [],
 		playerObjectsArray: [],
 		objArray: [],
+		buttons: [],
 
 		movingRightFlag: false,
 		movingLeftFlag: false,
 		collisionRightFlag: false,
 		collisionLeftFlag: false,
 		fallFlag: false,
+
+		ongoingTouches: {
+			0: 'doNothing',
+			1: 'doNothing',
+			2: 'doNothing',
+			3: 'doNothing',
+			4: 'doNothing',
+			5: 'doNothing'
+		}, //ongoingTouches
+
+		button: function (x,y,width,height,color,startAction,endAction) {
+			endAction = (endAction === undefined) ? 'doNothing' : endAction;
+
+			this.x = x * cSizing;
+			this.y = y * cSizing;
+			this.width = width * cSizing;
+			this.height = height * cSizing;
+			this.xVel = 0;
+			this.yVel = 0;
+			this.color = color;
+			this.startAction = startAction;
+			this.endAction = endAction;
+
+			this.top = gameObject.topBoundary;
+			this.bottom = gameObject.bottomBoundary;
+			this.left = gameObject.leftBoundary;
+			this.right = gameObject.rightBoundary;
+
+			this.drawSelf = function () {
+
+				ctx.fillStyle = this.color;
+				ctx.fillRect(this.x,this.y,this.width,this.height);
+			},
+
+			this.update = function () {
+
+				this.drawSelf();
+			}
+
+			gameObject.buttons.push(this);
+		},// backgroundObj
+
+		firePressed: function (){
+			new gameObject.fire();
+		},// firePressed
+
+		doNothing: function () {
+		},// doNothing
 
 		playerObj: function (x,y,width,height,color,iname1,file1,iname2,file2) {
 
@@ -411,6 +519,17 @@ var counter = 0;
 			}
 		},// checkCollisionLeft
 
+		rightPressed: function () {
+			console.log("pressed left");
+			if (gameObject.movingRightFlag === false){
+				gameObject.stopMovingLeft();
+				square.ft = 15;
+				square.ftMax = 216;
+				square.ftSize = 1;
+				gameObject.movingRightFlag = true;
+			}
+		},// leftPressed
+
 		moveLeft: function (obj) {
 
 			if (obj.xVel > -obj.maxVelocity){
@@ -422,6 +541,16 @@ var counter = 0;
 			// obj.ftSize = Math.floor(3 + Math.abs(obj.xVel) / (gameObject.ftSizeConstant));
 			obj.ftSize = Math.floor(3 + Math.abs(obj.xVel) / (temp)); 
 		},// moveLeft
+
+		leftPressed: function () {
+			if (gameObject.movingLeftFlag === false) {
+				gameObject.stopMovingRight();
+				square.ft = 15;
+				square.ftMax = 216;
+				square.ftSize = 1;
+				gameObject.movingLeftFlag = true;	
+			}
+		},// rightPressed
 
 		moveRight: function (obj) {
 
@@ -555,7 +684,10 @@ var counter = 0;
 
 				element.update();
 			});
+			gameObject.buttons.forEach(function (element) {
 
+				element.update();
+			});
 			requestAnimationFrame(gameObject.paintCanvas);
 		},// paintCanvas
 
@@ -611,6 +743,12 @@ var counter = 0;
 	new gameObject.backgroundObj (0,85,400,15,'brown');
 	new gameObject.backgroundObj (50,70,10,4,'pink');
 	new gameObject.backgroundObj (75,70,10,4,'yellow');
+
+	new gameObject.button (180,50,10,10,'gray','jump');
+	new gameObject.button (180,70,10,10,'gray','firePressed');
+	new gameObject.button (30,50,10,10,'gray','rightPressed','stopMovingRight');
+	new gameObject.button (10,50,10,10,'gray','leftPressed','stopMovingLeft');
+
 	
 
 
